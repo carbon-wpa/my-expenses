395724e05a0064a57257728092e2dd480e0ab672
Kacper Slotwinski <kacper.slotwinski@gmail.com>
2021-05-04 09:09:09 +0200

    settings tab

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,42 +31,46 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { useEffect, useState } from 'react';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+
+    const [sheetId, setSheetId] = useState<string>('');
+    const [apiKey, setApiKey] = useState<string>('');
+
+    useEffect( () => {
+        console.log('changed sheet Id ', sheetId);
+    }, [sheetId]);
+
+    return(
+        <IonApp>
+            <IonReactRouter>
+                <IonTabs>
+                    <IonRouterOutlet>
+                        <Route exact path="/tab1">
+                            <Tab1 />
+                        </Route>
+                        <Route exact path="/tab2">
+                            <Tab2 sheetId={sheetId} setSheetId={setSheetId} apiKey={apiKey} setApiKey={setApiKey} />
+                        </Route>
+                        <Route exact path="/">
+                            <Redirect to="/tab1" />
+                        </Route>
+                    </IonRouterOutlet>
+                    <IonTabBar slot="bottom">
+                        <IonTabButton tab="tab1" href="/tab1">
+                            <IonIcon icon={triangle} />
+                            <IonLabel>Tab 1</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="tab2" href="/tab2">
+                            <IonIcon icon={ellipse} />
+                            <IonLabel>Settings</IonLabel>
+                        </IonTabButton>
+                    </IonTabBar>
+                </IonTabs>
+            </IonReactRouter>
+        </IonApp>
+    );
+};
 
 export default App;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,68 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonInput,
+    IonButton, useIonToast
+} from '@ionic/react';
 import './Tab2.css';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+interface ISettings {
+    sheetId: string,
+    setSheetId: Dispatch<SetStateAction<string>>,
+    apiKey: string,
+    setApiKey: Dispatch<SetStateAction<string>>
+}
+
+const Tab2: React.FC<ISettings> = ({sheetId, setSheetId, apiKey, setApiKey}) => {
+
+    const [inputSheetId, setInputSheetId] = useState<string>(sheetId);
+    const [inputApiKey, setInputApiKey] = useState<string>(sheetId);
+
+    const [present, dismiss] = useIonToast();
+
+    const onSaveChanges = () => {
+        setSheetId(inputSheetId);
+        setApiKey(inputApiKey);
+        present({
+            message: 'Changes has been successfully saved!',
+            color: 'success',
+            duration: 2000
+        })
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Settings</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen className={'ion-padding'}>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Settings</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonItem>
+                    <IonLabel position="floating">Enter Sheet ID</IonLabel>
+                    <IonInput value={inputSheetId}
+                              onIonChange={(e) => setInputSheetId((e.target as HTMLInputElement).value)}></IonInput>
+                </IonItem>
+                <IonItem>
+                    <IonLabel position="floating">Enter API key</IonLabel>
+                    <IonInput value={inputApiKey}
+                              onIonChange={(e) => setInputApiKey((e.target as HTMLInputElement).value)}></IonInput>
+                </IonItem>
+                <IonButton expand="full" color="primary" onClick={() => onSaveChanges()}>Save Changes</IonButton>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab2;
